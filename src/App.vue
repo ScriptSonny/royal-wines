da6680306c07a52ba3889be7ab9cdd2e5e884164
ScriptSonny <rhsvbo@outlook.com>
2025-03-20 12:16:57 +0100

    implemented footer
    responsive

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import MainHeader from './components/MainHeader.vue';
+import MainFooter from './components/MainFooter.vue';
 </script>
 
 <template>
+  <div class="app-grid">
   <MainHeader />
 
+  <main class="main-content">
   <RouterView />
+  </main>
+  <MainFooter />
+  </div>
 </template>
 
 <style scoped>
-header {
-  max-width: none;
+.app-grid {
+  display: grid;
+  grid-template-rows: auto 1fr auto; /* Header auto, Content expands, Footer auto */
+  min-height: 100vh;
+}
+
+.main-content {
+  padding: 20px;
+}
+header, footer {
   width: 100%;
 }
 </style>
