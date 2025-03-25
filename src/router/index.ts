efa3a9540e3d790985b87a623cebec7023a543ed
JaafarJawadi29 <91067539+JaafarJawadi29@users.noreply.github.com>
2025-03-25 17:11:38 +0100

    begin kerstpakketen

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ChristmasPackView from '@/views/ChristmasPackView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,6 +18,10 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/kerstpakketten', 
+      component: ChristmasPackView,
+    }
   ],
 })
 
