16e84c0084303246ecb06545d27b04a2e2c40d05
JaafarJawadi29 <91067539+JaafarJawadi29@users.noreply.github.com>
2025-03-25 17:32:46 +0100

    kleine verandering

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,9 @@
-import HomepageView from '@/views/HomepageView.vue';
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import ChristmasPackView from '@/views/ChristmasPackView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
     {
       path: '/kerstpakketten', 
       component: ChristmasPackView,
