9ca2c175ea1e6dbb59bf0393e3cc32a0f9db60c5
JaafarJawadi29 <91067539+JaafarJawadi29@users.noreply.github.com>
2025-03-26 10:34:53 +0100

    snelle update

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,21 @@
+import ChristmasPackView from '@/views/ChristmasPackView.vue';
+import HomepageView from '@/views/HomepageView.vue';
 import { createRouter, createWebHistory } from 'vue-router'
-import ChristmasPackView from '@/views/ChristmasPackView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/kerstpakketten', 
-      component: ChristmasPackView,
+      path: '/',
+      name: 'Home',
+      component: HomepageView
+    },
+    {
+      path: '/',
+      name: 'Kerstpakketten',
+      component: ChristmasPackView
     }
-  ],
-})
+  ]
+});
 
 export default router
