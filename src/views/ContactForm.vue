b5a754130aed89a7429b47ee9094072699bb24a5
ScriptSonny <rhsvbo@outlook.com>
2025-04-03 14:23:28 +0200

    removed unnecessary component

--- src/views/ContactForm.vue
+++ src/views/ContactForm.vue
@@ -1,49 +1,153 @@
 <template>
     <div>
-        <main>
-            <h2 class="title">Contact</h2>
-            <div class="form-container">
-                <Form :title="title" :type="type" />
-            </div>
-        </main>
+      <main>
+        <h2 class="title">Contact</h2>
+        <div class="form-container">
+          <div class="form-card">
+            <h3 class="form-title">Contact</h3>
+            <form>
+              <label for="name">Naam</label>
+              <div class="input-container">
+                <i class="fa-solid fa-user"></i>
+                <input type="text" placeholder="Naam" />
+              </div>
+  
+              <label for="email">E-mailadres</label>
+              <div class="input-container">
+                <i class="fa-regular fa-envelope"></i>
+                <input type="email" placeholder="E-mailadres" />
+              </div>
+  
+              <label for="message">Type bericht</label>
+              <select>
+                <option>{{ type }}</option>
+              </select>
+  
+              <label for="files">Bijlagen</label>
+              <div class="upload-container">
+                <input type="file" />
+                <button class="upload-btn" type="button">Upload</button>
+              </div>
+  
+              <label for="extraMessage">Uw opmerking</label>
+              <textarea></textarea>
+  
+              <div class="submit">
+                <button class="submit-btn" type="submit"><h3>VERSTUREN</h3></button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </main>
     </div>
-</template>
-
-<script>
-import Form from '@/components/Form.vue';
-
-export default {
+  </template>
+  
+  <script>
+  export default {
     props: {
-        type: {
-            type: String,
-            required: true
-        }
-    },
-    components: { Form }
-};
-</script>
-
-<style scoped>
-main 
-{
+      type: {
+        type: String,
+        required: true
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  @import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css');
+  
+  main {
     width: 980px;
     margin: 0 auto;
-}
-
-.title
-{
+  }
+  
+  .title {
     margin: 20px 0;
     color: #5A2D2E;
     font-size: 32px;
     font-weight: 600;
     padding: 0 20px;
-}
-
-.form-container
-{
+  }
+  
+  .form-container {
     display: flex;
     justify-content: center;
     padding-bottom: 50px;
-}
-
-</style>
+  }
+  
+  .form-card {
+    background-color: #FAF4E5;
+    padding: 2rem;
+    box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+    width: 439px;
+  }
+  
+  .form-title {
+    font-size: 20px;
+    color: #663333;
+    font-weight: 600;
+    margin-bottom: 1rem;
+  }
+  
+  label {
+    display: block;
+    margin-top: 1rem;
+    font-weight: 500;
+    font-size: 15px;
+    color: #663333;
+  }
+  
+  input,
+  select,
+  textarea {
+    width: 100%;
+    padding: 0.5rem;
+    margin-top: 0.5rem;
+    border: 1px solid #663333;
+    background-color: #FAF4E5;
+  }
+  
+  .input-container {
+    position: relative;
+    display: flex;
+    align-items: center;
+  }
+  
+  .input-container i {
+    position: absolute;
+    left: 10px;
+    color: #663333;
+  }
+  
+  .input-container input {
+    padding-left: 30px;
+  }
+  
+  .upload-container {
+    display: flex;
+    align-items: flex-end;
+  }
+  
+  .upload-btn {
+    margin-left: 0.5rem;
+    padding: 0.6rem 1rem;
+    background-color: #663333;
+    color: white;
+    border: none;
+  }
+  
+  .submit {
+    display: flex;
+    flex-direction: row-reverse;
+  }
+  
+  .submit-btn {
+    border: 2px solid #E59F01;
+    background-color: #FAF4E5;
+    color: #E59F01;
+    margin-top: 0.5rem;
+    padding: 10px 18px;
+    cursor: pointer;
+  }
+  </style>
+  
